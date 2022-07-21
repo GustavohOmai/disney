a404fb20a2586bd04330efc9dfead4eba43fb05b
Breno Tosi <breno_tosi@hotmail.com>
2022-07-21 20:10:44 -0300

    SAPOHA DE HEADER TA PRONTO

---
+++ script.js
@@ -0,0 +1,14 @@
+const sandwich = document.querySelector(".sandwich");
+const navMenu = document.querySelector(".nav-menu");
+
+sandwich.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+}))
+
+document.querySelectorAll(".nav-link2").forEach(n => n.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+}))
